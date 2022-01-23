a627bffbb9c1a0858b62b8eb9f0a7bbd860f03f8
AdrianSadowski <sadow.adrian@gmail.com>
2022-01-23 16:33:08 +0100

    Add formatFullName tests

--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -2,14 +2,12 @@ const cutText = require('../cutText.js');
 const expect = require('chai').expect;
 
 describe('CutText', () => {
-  let concent = 'Lorem Ipsum';
   it('should return an error if "concent" arg is not a string', () => {
-    if(typeof content !== 'string') return 'Error';
     expect(cutText(undefined, 20)).to.equal('Error');
     expect(cutText(12, 20)).to.equal('Error');
     expect(cutText({}, 20)).to.equal('Error');
     expect(cutText([], 20)).to.equal('Error');
-    expect(cutText(function() {}, 20)).toEqual('Error');
+    expect(cutText(function() {}, 20)).to.equal('Error');
   });
 
   it('should return an error if "concent" arg length is 0', () => {
@@ -17,19 +15,17 @@ describe('CutText', () => {
   });
 
   it('should return an error if "maxLenght" arg is not a number', () => {
-    if(typeof maxLength !== 'string') return 'Error';
-    expect(cutText(concent, undefined)).to.equal('Error');
-    expect(cutText(concent, 'abc')).to.equal('Error');
-    expect(cutText(concent, {})).to.equal('Error');
-    expect(cutText(concent, [])).to.equal('Error');
-    expect(cutText(concent, function() {})).to.equal('Error');
+    expect(cutText('Lorem Ipsum', undefined)).to.equal('Error');
+    expect(cutText('Lorem Ipsum', 'abc')).to.equal('Error');
+    expect(cutText('Lorem Ipsum', {})).to.equal('Error');
+    expect(cutText('Lorem Ipsum', [])).to.equal('Error');
+    expect(cutText('Lorem Ipsum', function() {})).to.equal('Error');
   });
 
-  // it('should return an error if "maxLength" is lower or equal 0', () => {
-  //   if(maxLength <= 0) return 'Error';
-  //   expect(cutText(concent, 0)).to.equal('Error');
-  //   expect(cutText(concent, -6)).to.equal('Error');
-  // });
+  it('should return an error if "maxLength" is lower or equal 0', () => {
+    expect(cutText('Lorem Ipsum', 0)).to.equal('Error');
+    expect(cutText('Lorem Ipsum', -6)).to.equal('Error');
+  });
 
   it('should return "content" without changes if proper args', () => {
     expect(cutText('Lorem Ipsum', 40)).to.equal('Lorem Ipsum');
